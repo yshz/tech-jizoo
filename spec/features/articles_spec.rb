4a6db13c7f00d93c316ba8e76d781633ef4b9cae
jizoo <jizoo2200@gmail.com>
2014-08-13 19:22:42 +0900

    Apply basic design for layout

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -5,7 +5,10 @@ feature '記事一覧の表示' do
     Article.create_articles
     visit root_path
 
+    expect(page).to have_title('jizoo blog')
+    expect(page).to have_link('jizoo blog')
     expect(page).to have_css('li', text: 'Lorem ipsum')
+    expect(page).to have_css('footer', text: 'jizoo')
   end
 end
 
