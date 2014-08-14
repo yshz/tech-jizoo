434d2cc22ad8ac7533ec059e783590bd940f0dbc
jizoo <jizoo2200@gmail.com>
2014-08-14 09:28:30 +0900

    Set up default timezone

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ Bundler.require(*Rails.groups)
 module TechJizoo
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
       g.helper false
       g.assets false
