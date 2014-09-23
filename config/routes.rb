4386872279add5ec89c15902762e57dbd3b741e5
jizoo <jizoo2200@gmail.com>
2014-09-23 14:32:23 +0900

    Fix ArticleView#show to change params from title to slug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  get '/:year/:month/:day/:title', to: 'articles#show', as: :article
+  get '/:year/:month/:day/:slug', to: 'articles#show', as: :article
 
   get '*anything', to: redirect('/')
 end
